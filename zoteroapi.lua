c8b51005b43b6b296135505ec17c50c73a70356f
Jochen Arlt <j.arlt@ed.ac.uk>
2024-12-02 22:25:45 +0000

    Show path in browser

--- zoteroapi.lua
+++ zoteroapi.lua
@@ -24,7 +24,7 @@ local Geom = require("ui/geometry")
 -- /storage/<KEY>/version: Version number of downloaded attachment
 -- /meta.lua: Metadata containing library version, items etc.
 
-local API = { ["version"] = "JA v1.0 RC"}
+local API = { ["version"] = "JA v1.0 dev"}
 
 local SUPPORTED_MEDIA_TYPES = {
     [1] = "application/pdf",
